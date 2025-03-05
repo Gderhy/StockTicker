d9daedbfdd14132ba11b55cb2936079d3d744453
Gderhy <gabrielderhy@gmail.com>
2025-03-05 15:22:35 -0500

    Add fetchCompanies service to retrieve company data and symbols

---
+++ backend/src/DAO/services/fetchCompanies.js
@@ -0,0 +1,25 @@
+import Company from "../models/Company";
+
+// Function to fetch all companies
+async function fetchCompanies() {
+  try {
+    const companies = await Company.find();
+    return companies;
+  } catch (error) {
+    console.error("Error fetching companies:", error);
+    return [];
+  }
+}
+
+async function fetchCompanySymbols() {
+  // Returns an array of company symbols
+  try {
+    const symbols = await Company.find().select("symbol");
+    return symbols;
+  } catch (error) {
+    console.error("Error fetching companies:", error);
+    return [];
+  }
+}
+
+module.exports = { fetchCompanies, fetchCompanySymbols };
