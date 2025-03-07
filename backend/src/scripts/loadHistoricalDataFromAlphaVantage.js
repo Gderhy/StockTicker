132327a1481091e1baed1f38ff309ec18bee40ba
Gderhy <gabrielderhy@gmail.com>
2025-03-06 23:41:47 -0500

    Add loadHistoricalDataFromAlphaVantage script: implement historical data loading and clear existing data

---
+++ backend/src/scripts/loadHistoricalDataFromAlphaVantage.js
@@ -0,0 +1,56 @@
+const { connectDB, disconnectDB } = require("../db/mongoDb");
+const CompanyModel = require("../models/Company");
+const { getStockModels, getStockModel } = require("../db/models/Stock");
+const { fetchAlphaVantageData } = require("../services/fetchAlphaVantage");
+const { clearModelFromDb } = require("./helperFunctions/clearModelFromDb");
+
+// Stock symbols to fetch data for
+const stocks = [
+  { symbol: "AAPL", companyName: "Apple" },
+  { symbol: "GOOGL", companyName: "Google" },
+  { symbol: "MSFT", companyName: "Microsoft" },
+  { symbol: "VPY", companyName: "Valpay" },
+];
+
+// Function to load historical data into MongoDB
+async function loadHistoricalData() {
+  try {
+      await connectDB();
+  
+      // Clear the db
+      // Optional: Clear existing data
+      await clearModelFromDb(CompanyModel);
+  
+      // Get the stock model for each symbol
+      const stockSymbols = stocks.map((stock) => stock.symbol);
+      const StockModels = getStockModels(stockSymbols);
+  
+      // Clears each stock collection
+      await Promise.all(
+        StockModels.map(async (StockModel) => {
+          clearModelFromDb(StockModel);
+        })
+      );
+  
+      await Promise.all(
+        stocks.map(async ({ symbol }) => {
+          const StockModel = getStockModel(symbol);
+  
+          // Fetch data from Polygon
+          const historicalData = await fetchAlphaVantageData(symbol);
+          if (!historicalData.length) return;
+  
+          // Insert data into MongoDB
+          await StockModel.insertMany(historicalData);
+          console.log(`Inserted historical data for ${symbol}.`);
+        })
+      );
+  } catch (error) {
+    console.error("Error loading historical data:", error);
+  } finally {
+    await disconnectDB();
+  }
+}
+
+// Run the script
+loadHistoricalData();
